d69adc435406f0f943dce45b3f69d42cc5d5286d
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-26 15:55:47 -0700

    comment out test for fetchCoordsByIP now that it's working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -6,4 +8,12 @@ const { fetchMyIP } = require('./iss');
 //     return;
 //   }
 //   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP("162.245.144.188", (error, data) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   console.log('Your location data is:', data);
 // });
