af6777adca3e499f3bd0aeec639fb91038351530
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-26 15:13:17 -0700

    remove unnecessary return statement and refactor if (error) statement to be one line

--- iss.js
+++ iss.js
@@ -10,10 +10,7 @@ const request = require('request');
 
 const fetchMyIP = function(callback) {
   request("https://api.ipify.org/?format=json", (error, response, body) => {
-    if (error) {
-      callback(error, null);
-      return;
-    }
+    if (error) return callback(error, null);
     
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
@@ -22,7 +19,7 @@ const fetchMyIP = function(callback) {
     }
 
     const data = JSON.parse(body);
-    return callback(null, data.ip);
+    callback(null, data.ip);
   });
 };
 
