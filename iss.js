0c0e60778a50e395ebb137ebbd2fe75ea8c25553
charcharmasonjar <charlotte.dibb@gmail.com>
2019-09-26 15:56:22 -0700

    create fetchCordsByIP function that gets latitude and longitude based off of IP address

--- iss.js
+++ iss.js
@@ -23,4 +23,20 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  const URL = "https://ipvigilante.com/" + ip;
+  request(URL, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP location. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const { latitude, longitude } = JSON.parse(body).data;
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
